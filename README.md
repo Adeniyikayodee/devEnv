88435c57b4ab30223a3a324c8f9a662cdc850536
kayode <adeniyikayode22@gmail.com>
2022-07-24 14:34:27 +0100

    set up

---
+++ README.md
@@ -0,0 +1,9 @@
+## Dev Environment With Terraform and AWS
+
+## Prerequsities 
+
+1. AWS Account and cli installed
+2. Install Terraform
+3. 
+
+## Usage
---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  // shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  shared_credentials_files = "~/.aws/credentials"
+  profile                  = "vscode"
+}
